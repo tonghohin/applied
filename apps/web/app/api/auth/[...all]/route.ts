789b76f03ff41eb2b410725f2bacca078d104b19
Hin <tonghohin77@gmail.com>
2026-06-25 00:12:50 -0400

    refactor: update auth handler to use toNextJsHandler for GET and POST methods

--- apps/web/app/api/auth/[...all]/route.ts
+++ apps/web/app/api/auth/[...all]/route.ts
@@ -1,4 +1,4 @@
 import { auth } from "@repo/api";
+import { toNextJsHandler } from "better-auth/next-js";
 
-export const GET = auth.handler;
-export const POST = auth.handler;
+export const { GET, POST } = toNextJsHandler(auth);
